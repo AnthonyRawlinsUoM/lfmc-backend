571dc67b416c17673700f4b00c4b28048b63298d
AnthonyRawlinsUoM <rawlinsfamily@icloud.com>
2019-06-18 15:15:36 +1000

    version 10.0.24

--- serve/lfmc/query/GeoQuery.py
+++ serve/lfmc/query/GeoQuery.py
@@ -33,6 +33,10 @@ class GeoQuery(ShapeQuery):
     vicgrid94 = pyproj.Proj("+init=EPSG:3111")
     wgs84 = pyproj.Proj("+init=EPSG:4326")
 
+    def __init__(self, start, finish, geo_json):
+        self.query = ShapeQuery(start, finish, geo_json)
+        self.idx = None
+
     def __init__(self, shape_query):
         self.query = shape_query
         self.idx = None
